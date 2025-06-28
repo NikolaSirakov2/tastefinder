818803de3500c5a8f63d19c274dd73821adbda6e
NikolaSirakov2 <nikolasirakov1@gmail.com>
2025-06-28 16:28:30 +0300

    Add 3D card component and integrate it into Home page; update package dependencies for motion effects

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,68 @@
+"use client";
+
+import React from "react";
 import { Button } from "@/components/ui/button";
+import { CardBody, CardContainer, CardItem } from "@/components/ui/3d-card";
+
+export function ThreeDCardDemo() {
+  return (
+    <CardContainer className="inter-var">
+      <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+        <CardItem
+          translateZ="20"
+          className="text-xl font-bold text-neutral-600 dark:text-white"
+        >
+          Taste Finder
+        </CardItem>
+        <CardItem
+          as="p"
+          translateZ="25"
+          className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+        >
+          Find the best deals on clothing and gifts.
+        </CardItem>
+        <CardItem
+          translateZ="2"
+          rotateX={0}
+          rotateZ={0}
+          className="w-full mt-4"
+        >
+          <img
+            src="/shopping.png"
+            height="1000"
+            width="1000"
+            className="h-80 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+            alt="Grocery shopping"
+          />
+        </CardItem>
+        <div className="flex justify-between items-center mt-20">
+          <CardItem
+            translateZ={5}
+            translateX={-5}
+            as="button"
+            className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+          >
+            Try now →
+          </CardItem>
+          <CardItem
+            translateZ={5}
+            translateX={5}
+            as="button"
+            className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
+          >
+            Sign up
+          </CardItem>
+        </div>
+      </CardBody>
+    </CardContainer>
+  );
+}
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1 className="text-4xl font-bold">Taste Finder</h1>
-        <p className="text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)] tracking-[-.01em]">
-          Find the best deals on flights, hotels, and car rentals.
-        </p>
-
-        <Button>Take a quiz</Button>
+        <ThreeDCardDemo />
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
